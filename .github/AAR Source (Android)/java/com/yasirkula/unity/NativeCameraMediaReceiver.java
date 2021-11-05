fd685bf6f07cb4f021fe5a43f67640c2b0ae4cf5
DESKTOP-V74UB9B\Star <devmastery@outlook.com>
2021-11-05 10:45:39 -0700

    modify some parts

---
+++ .github/AAR Source (Android)/java/com/yasirkula/unity/NativeCameraMediaReceiver.java
@@ -0,0 +1,10 @@
+package com.devmastery.unity;
+
+/**
+ * Created by devmastery on 22.04.2018.
+ */
+
+public interface NativeCameraMediaReceiver
+{
+	void OnMediaReceived( String path );
+}
---
+++ .github/AAR Source (Android)/java/com/yasirkula/unity/NativeCameraPermissionReceiver.java
@@ -0,0 +1,10 @@
+package com.devmastery.unity;
+
+/**
+ * Created by devmastery on 5.03.2018.
+ */
+
+public interface NativeCameraPermissionReceiver
+{
+	void OnPermissionResult( int result );
+}
